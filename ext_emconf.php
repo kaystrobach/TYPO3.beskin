a3edb563c6d6dd4882ea3d0af77ac841fe353513
Kay Strobach <github@kay-strobach.de>
2013-03-12 21:51:22 +0100

    TER Upload

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,22 +1,22 @@
 <?php
 
-########################################################################
-# Extension Manager/Repository config file for ext "beskin".
-#
-# Auto generated 04-05-2012 18:12
-#
-# Manual updates:
-# Only the data in the array - everything else is removed by next
-# writing. "version" and "dependencies" must not be touched!
-########################################################################
+/***************************************************************
+ * Extension Manager/Repository config file for ext "beskin".
+ *
+ * Auto generated 11-03-2013 19:40
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
 
-$EM_CONF[$_EXTKEY] = array(
+$EM_CONF[$_EXTKEY] = array (
 	'title' => 'TYPO3 easy beskin Extensions',
 	'description' => '',
 	'category' => '',
 	'shy' => 0,
-	'version' => '1.1.2',
-	'dependencies' => 't3skin',
+	'version' => '1.1.3',
+	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
 	'loadOrder' => '',
@@ -32,18 +32,23 @@ $EM_CONF[$_EXTKEY] = array(
 	'author_company' => '',
 	'CGLcompliance' => '',
 	'CGLcompliance_note' => '',
-	'constraints' => array(
-		'depends' => array(
+	'constraints' => 
+	array (
+		'depends' => 
+		array (
 			'php' => '5.1.0-0.0.0',
-			'typo3' => '4.4.0-0.0.0',
+			'typo3' => '4.5.0-6.0.99',
 		),
-		'conflicts' => array(
+		'conflicts' => 
+		array (
 		),
-		'suggests' => array(
+		'suggests' => 
+		array (
 		),
 	),
 	'_md5_values_when_last_written' => 'a:8:{s:17:"addStylesheet.php";s:4:"989b";s:21:"ext_conf_template.txt";s:4:"6b61";s:12:"ext_icon.gif";s:4:"6759";s:17:"ext_localconf.php";s:4:"c33c";s:14:"ext_tables.php";s:4:"51a4";s:29:"Classes/Hook/TYPO3Backend.php";s:4:"928f";s:14:"doc/manual.sxw";s:4:"c468";s:23:"stylesheets/changes.css";s:4:"aeac";}',
-	'suggests' => array(
+	'suggests' => 
+	array (
 	),
 );
 
