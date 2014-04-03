10615f455dcebebc0fc5fa414212cd346ee4787a
Kay Strobach <github@kay-strobach.de>
2014-04-03 17:20:46 +0200

    raise version to 6.2

--- ext_emconf.php
+++ ext_emconf.php
@@ -37,7 +37,7 @@ $EM_CONF[$_EXTKEY] = array (
 		'depends' => 
 		array (
 			'php' => '5.1.0-0.0.0',
-			'typo3' => '4.5.0-6.0.99',
+			'typo3' => '4.5.0-6.2.99',
 		),
 		'conflicts' => 
 		array (
